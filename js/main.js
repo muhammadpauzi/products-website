2278710ab91d669ef5ea813c76aa0ae457e8139f
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-09-16 13:29:55 +0700

    update UI

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-const menu = document.querySelector('.navbar .ham-menu');
-const navbarList = document.querySelector('.navbar .navbar-list');
+const navbar = document.querySelector('.navbar');
+const menu = navbar.querySelector('.ham-menu');
+const navbarList = navbar.querySelector('.navbar-list');
+
+function showStickyNavbar() {
+    if (this.scrollY > 0) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+}
+
+showStickyNavbar();
+
+window.addEventListener('scroll', showStickyNavbar)
 
 menu.addEventListener('click', function () {
-    this.firstElementChild.src = navbarList.classList.toggle('active') ? "./icons/x.svg" : "./icons/menu.svg";
+    // this.firstElementChild.src = navbarList.classList.toggle('active') ? "./icons/x.svg" : "./icons/menu.svg";
+    navbarList.classList.toggle('active');
 });
